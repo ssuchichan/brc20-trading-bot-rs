90fdf743795d3547c8a31c6417c8d06d0d637cb2
ssuchichan <ssuchichan@gmail.com>
2024-05-01 08:11:14 -0700

    update

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,7 @@ use clap::Parser;
 use dotenv::dotenv;
 use env_logger::Target;
 use log::info;
+use serde_json::from_str;
 use sqlx::pool::PoolOptions;
 use sqlx::{PgPool, Pool, Postgres};
 use std::io::Read;
@@ -142,7 +143,15 @@ async fn main() -> Result<()> {
     let ex_rpc_url = env::var("EX_RPC")?;
     let node_rpc_url = env::var("NODE_RPC")?;
     let node_api_port = env::var("NODE_API_PORT")?;
+    let list_sum_amount = from_str::<u64>(&env::var("LIST_SUM_AMOUNT")?)?;
+
     let rpc = Rpc::new(&ex_rpc_url, &format!("{}:{}", node_rpc_url, node_api_port))?;
+
+    let floor_prices: Vec<u64> = vec![
+        123000000, 250000000, 450000000, 200000000, 220000000, 300000000,
+    ];
+    let mut price_index = 1;
+    let mut account_index = 0;
 
     let server = BotServer::new(pool, rpc, accounts_mint, accounts_buy)?;
     server.prepare_accounts().await?;
@@ -154,14 +163,64 @@ async fn main() -> Result<()> {
         tokio::select! {
             _ = timer1.tick() => {
                 let list_res = server.get_token_list(&token, 1, 50).await?;
-                if let Some(lists) = list_res.data {
-                    println!("total lists: {}", lists.len());
+                if list_res.total == 0 {
+                    println!("[List] no lists");
+                    continue;
                 }
-                println!("No lists");
+                let mut sum = 0;
+                for item in list_res.data.unwrap() {
+                    sum += from_str::<u64>(&item.amount)?;
+                }
+                let pages = list_res.total / 50 + 1;
+                for page in 1..pages {
+                    let list_res = server.get_token_list(&token, page, 50).await?;
+                    if list_res.total == 0 {
+                        continue;
+                    }
+                    for item in list_res.data.unwrap() {
+                        sum += from_str::<u64>(&item.amount)?;
+                    }
+                }
+
+                if sum >= list_sum_amount{
+                    continue;
+                }
+                println!("[List] add lists");
+                todo!()
             },
             _ = timer2.tick() => {
-                let list_res = server.get_token_list(&token, 1, 50).await?;
+                let cur_floor_price = floor_prices[price_index%floor_prices.len()];
 
+                let list_res = server.get_token_list(&token, 1, 50).await?;
+                if list_res.total == 0 {
+                    println!("[buy] no lists");
+                    continue;
+                }
+                let pages = list_res.total / 50 + 1;
+                if let Some(items) = list_res.data {
+                    for i in 0..items.len() {
+                        let price = from_str::<u64>(&items[i].price)?;
+                        if price <= cur_floor_price {
+                            todo!()
+                        }
+                    }
+                }
+                for page in 1..pages {
+                    let list_res = server.get_token_list(&token, page, 50).await?;
+                    if list_res.total == 0 {
+                        println!("[List] no lists");
+                        continue;
+                    }
+
+                    println!("total lists: {}", list_res.total);
+                    if let Some(items) = list_res.data {
+                        for i in 0..items.len() {
+                            todo!()
+                        }
+                    }
+                }
+
+                price_index += 1;
             }
         }
     }
